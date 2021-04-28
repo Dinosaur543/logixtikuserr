64cf3c703c3c573c7eed9e32dc0067521dbacfe8
Saowaluk Kaewnok <sou.spb@gmail.com>
2021-04-28 23:41:17 +0700

    first

---
+++ src/components/RouteMap/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
+
+const GOOGLE_MAPS_APIKEY = 'AIzaSyDOgLD-43H5RYYNbi-VCgFC30rG-AMX5vQ';
+
+const RouteMap = (props) => {
+
+  const origin = {
+    latitude: 8.2815357, //8.2815357,99.9082777
+    longitude: 99.9082777,
+  };
+
+  const destination = {
+    latitude: 8.1795872, //8.1795872,99.8604895
+    longitude: 99.8604895,
+  };
+
+  const originLoc = {
+    latitude: origin.details.geometry.location.lat,
+    longitude: origin.details.geometry.location.lng,
+  };
+
+  const destinationLoc = {
+    latitude: destination.details.geometry.location.lat,
+    longitude: destination.details.geometry.location.lng,
+  };
+
+  return (
+    <MapView
+      style={{width: '100%', height: '100%'}}
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation={true}
+      initialRegion={{
+        latitude: 13.7563,
+        longitude:100.5018,
+        latitudeDelta: 0.0222,
+        longitudeDelta: 0.0121,
+      }}>
+      <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_MAPS_APIKEY}
+        strokeWidth={5}
+        strokeColor="red"
+      />
+      <Marker
+        coordinate={origin}
+        title={'Origin'}
+      />
+      <Marker
+        coordinate={destination}
+        title={"Destination"}
+      />
+    </MapView>
+  );
+};
+
+export default RouteMap;
