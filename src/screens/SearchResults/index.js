00a292205e1f6b71d1064a9fff9ceb254ab71a7e
Saowaluk Kaewnok <sou.spb@gmail.com>
2021-04-29 20:56:54 +0700

    before use AWS

--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {View, Dimensions} from 'react-native';
 import RouteMap from "../../components/RouteMap";
 import UberTypes from "../../components/UberTypes";
 
+import { useRoute, useNavigation } from '@react-navigation/native';
+
+
 const SearchResults = (props) => {
+  //const typeState = useState(null);
+
+  const route = useRoute();
+  const navigation = useNavigation();
+
+
+  const {originPlace, destinationPlace} = route.params
+  console.log(route.params)
+
   return (
     <View style={{display: 'flex', justifyContent: 'space-between'}}>
       <View style={{height: Dimensions.get('window').height - 400}}>
-        <RouteMap />
+        <RouteMap origin={originPlace} destination={destinationPlace}/>
       </View>
 
       <View style={{height: 400}}>
